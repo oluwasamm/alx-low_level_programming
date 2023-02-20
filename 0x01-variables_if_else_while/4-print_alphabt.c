d5419db3077ba227ad92919ff8a952877fb66e77
Akinwale <samuelakinwale73@gmail.com>
2023-02-20 15:31:56 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - print all alphabet except q and e
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		if (i != 101 && i != 133)
+		{
+			putchar(i);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
