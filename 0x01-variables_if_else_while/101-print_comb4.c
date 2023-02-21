1b246966aa8bc7384cc39138f5b2bc242ffb8732
Akinwale <samuelakinwale73@gmail.com>
2023-02-20 19:48:38 -0800

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,15 +6,15 @@
  */
 int main(void)
 {
-        int n, m, l;
+	int n, m, l;
 
-        for (n = 48; n < 58; n++)
-        {
-                for (m = 49; m < 58; m++)
-                {
+	for (n = 48; n < 58; n++)
+	{
+		for (m = 49; m < 58; m++)
+		{
 			for (l = 50; l < 58; l++)
 			{
-                                if (l > m && m > n)
+				if (l > m && m > n)
 				{
 					putchar(n);
 					putchar(m);
@@ -29,7 +29,5 @@ int main(void)
 		}
 	}
 	putchar('\n');
-	putchar (0);
+	return (0);
 }
-
-
